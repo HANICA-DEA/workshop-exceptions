ce7c2b0532ee96062c1a7c3594316f14fc559c27
Rody Middelkoop <rody.middelkoop@gmail.com>
2021-09-06 14:01:41 +0200

    Added to the uitwerking branch after having it tested in classroom

---
+++ src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/NegativeInputException.java
@@ -0,0 +1,7 @@
+package nl.han.ica.oose.dea.testedfizzbuzz;
+
+public class NegativeInputException extends RuntimeException {
+    public NegativeInputException(String message) {
+        super(message);
+    }
+}
